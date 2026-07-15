4b08540f7fd550d97aac0cef8d8f8159653f122e
akbar1362 <akbar1362@users.noreply.github.com>
2026-07-15 00:23:18 -0700

    fix: serve media files in production using django.views.static.serve

--- moviedl/urls.py
+++ moviedl/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from core.views import health_check
 
 urlpatterns = [
@@ -12,4 +12,11 @@ urlpatterns = [
     path('core/', include('core.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]

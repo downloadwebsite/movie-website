519a4c8acd48cdbf21a0161a76f95a7379fbe209
akbar1362 <akbar1362@users.noreply.github.com>
2026-07-14 23:53:41 -0700

    fix: support Persian/Unicode slugs in URLs

--- movies/urls.py
+++ movies/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'movies'
@@ -6,15 +6,15 @@ app_name = 'movies'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('movies/', views.MovieListView.as_view(), name='movie_list'),
-    path('movie/<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'),
-    path('category/<slug:slug>/', views.CategoryDetailView.as_view(), name='category_detail'),
+    re_path(r'^movie/(?P<slug>[\w\u0600-\u06FF-]+)/$', views.MovieDetailView.as_view(), name='movie_detail'),
+    re_path(r'^category/(?P<slug>[\w\u0600-\u06FF-]+)/$', views.CategoryDetailView.as_view(), name='category_detail'),
     path('search/', views.SearchView.as_view(), name='search'),
     path('series/', views.SeriesListView.as_view(), name='series_list'),
     path('top-rated/', views.TopRatedView.as_view(), name='top_rated'),
 
     # AJAX views
-    path('movie/<slug:slug>/comment/', views.add_comment, name='add_comment'),
-    path('movie/<slug:slug>/rating/', views.add_rating, name='add_rating'),
-    path('movie/<slug:slug>/favorite/', views.toggle_favorite, name='toggle_favorite'),
-    path('movie/<slug:slug>/download/<int:link_id>/', views.download_movie, name='download_movie'),
+    re_path(r'^movie/(?P<slug>[\w\u0600-\u06FF-]+)/comment/$', views.add_comment, name='add_comment'),
+    re_path(r'^movie/(?P<slug>[\w\u0600-\u06FF-]+)/rating/$', views.add_rating, name='add_rating'),
+    re_path(r'^movie/(?P<slug>[\w\u0600-\u06FF-]+)/favorite/$', views.toggle_favorite, name='toggle_favorite'),
+    re_path(r'^movie/(?P<slug>[\w\u0600-\u06FF-]+)/download/(?P<link_id>\d+)/$', views.download_movie, name='download_movie'),
 ]

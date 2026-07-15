4f7c995dec399ab88fe246a4ea0256519a5941db
akbar1362 <akbar1362@users.noreply.github.com>
2026-07-14 21:47:18 -0700

    Add health check endpoint

--- moviedl/urls.py
+++ moviedl/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from core.views import health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', health_check, name='health_check'),
     path('', include('movies.urls')),
     path('accounts/', include('accounts.urls')),
     path('core/', include('core.urls')),
